b63e35a28b1a256f6491e81200c3fd225b4099f9
shadow-linq <133143219+shadow-linq@users.noreply.github.com>
2023-06-12 09:31:34 -0600

    Create azcreeper.py

---
+++ azcreeper.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import sys
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print("azcreeper.py <DOMAIN> <USERLIST>")
+  domain = sys.argv[1]
+  f = open(sys.argv[2], "r")
+  users = f.read().split('\n')
+  for user in users:
+    email = user
+    if "@" not in email:
+      email += "@" + domain
+      r = requests.post("https://login.microsoftonline.com/common/GetCredentialType", json={"Username":email})
+      r = json.loads(r.text)
+      if r["IfExistsResult"] == True or r["Credentials"]["CertAuthParams"] != None:
+        print(f"{email} exists")
